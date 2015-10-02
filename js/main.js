57f27e9b2f2088bc6a402ea8d670f157f8532217
Patrick Hund <pahund@team.mobile.de>
2015-10-02 17:49:44 +0200

    improved auto reload
    Change-Id: I515ec14f3fe333aae8799fe52ad77627f6d5a81a

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ function isScrolledDown() {
     return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
 }
 
+function hasAutoReload() {
+    return document.getElementById("autoreload").checked;
+}
+
 function withTimeout(ms) {
     return function (func) {
         window.setTimeout(func, ms);
@@ -11,25 +15,51 @@ function scrollDown() {
     window.scrollTo(0, document.body.scrollHeight);
 }
 
+function saveCookie(name, value) {
+    document.cookie = name + "=" + value;
+}
+
 function saveScrolledDown() {
-    document.cookie = "scrolledDown=" + isScrolledDown();
+    saveCookie("scrolledDown", isScrolledDown());
+}
+
+function saveAutoReload() {
+    saveCookie("autoReload", hasAutoReload());
+}
+
+function loadCookie(name) {
+    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(name).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) === "true";
+    //return document.cookie.replace("(?:(?:^|.*;\s*)" + name + "\s*\=\s*([^;]*).*$)|^.*$", "$1") === "true";
 }
 
 function wasScrolledDown() {
-    return document.cookie.replace(/(?:(?:^|.*;\s*)scrolledDown\s*\=\s*([^;]*).*$)|^.*$/, "$1") === "true";
+    return loadCookie("scrolledDown");
+}
+
+function hadAutoReload() {
+    return loadCookie("autoReload");
 }
 
 function reloadPage() {
     document.location.reload();
 }
 
-//(function () {
-//    if (wasScrolledDown()) {
-//        withTimeout(10)(scrollDown);
-//    }
-//    window.setInterval(
-//        function executeInterval() {
-//            saveScrolledDown();
-//            reloadPage();
-//        } , 1000);
-//}());
+(function () {
+    console.log("[PH_LOG] loadCookie('autoReload'): ", loadCookie('autoReload')); // PH_TODO: REMOVE
+    console.log("[PH_LOG] loadCookie('scrolledDown'): ", loadCookie('scrolledDown')); // PH_TODO: REMOVE
+    if (wasScrolledDown()) {
+        withTimeout(10)(scrollDown);
+    }
+    if (hadAutoReload()) {
+        console.log("[PH_LOG] phugg"); // PH_TODO: REMOVE
+        document.getElementById("autoreload").checked = true;
+    }
+    window.setInterval(
+        function executeInterval() {
+            if (hasAutoReload()) {
+                saveAutoReload();
+                saveScrolledDown();
+                reloadPage();
+            }
+        } , 1000);
+}());
