ff982e877bd6332b3fe55209f38c1e48970dbf3c
Patrick Hund <pahund@team.mobile.de>
2015-11-06 15:21:35 +0100

    Mocha/Chai/BDD code example
    Change-Id: Ic40d61de478aa4baaaf63614daaf8e69ba4a3f97

---
+++ demos/mocha-chai-bdd/test.js
@@ -0,0 +1,28 @@
+/**
+ * index.js
+ *
+ * @author <a href="mailto:pahund@team.mobile.de">Patrick Hund</a>
+ * @since 06 Nov 2015
+ */
+"use strict";
+
+const chai = require("chai"),
+    programmer = require("./programmer");
+
+let codeQuality;
+
+chai.should();
+
+describe("When I hire a programmer", () => {
+    describe("the code quality", () => {
+        before(() => codeQuality = programmer.getCodeQuality());
+        it("is crappy", () => codeQuality.should.equal("crappy"));
+    });
+    describe("and ask her to use test automation", () => {
+        before(() => programmer.useTestAutomation());
+        describe("the code quality", () => {
+            before(() => codeQuality = programmer.getCodeQuality());
+            it("is awesome", () => codeQuality.should.equal("awesome"));
+        });
+    });
+});
